20aa56c30d42da905f18bf031e2f6ad67eedd21a
Snobb3ry <ARPITPATHAK.200111021@gehu.ac.in>
2022-02-01 16:50:57 +0530

    Added train_dir and val_dir

--- Source/emojify.py
+++ Source/emojify.py
@@ -7,3 +7,23 @@ from keras.layers import Conv2D
 from keras.optimizers import adam_v2
 from keras.layers import MaxPooling2D
 from keras.preprocessing.image import ImageDataGenerator
+
+train_dir = 'data/train'
+val_dir = 'data/test'
+train_datagen = ImageDataGenerator(rescale=1.5/255)
+val_datagen = ImageDataGenerator(rescale=1.5/255)
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(48.48),
+    batch_size=64,
+    color_mode="greyscale",
+    class_mode='categorical',
+)
+validation_generator = val_datagen.flow_from_directory(
+    val_dir,
+    target_size=(48.48),
+    batch_size=64,
+    color_mode="greyscale",
+    class_mode='categorical',
+)
+
